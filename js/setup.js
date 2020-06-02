dfcf92a2abc0bb2e1062d1a90adc22bb03341fa7
julia-marta <marta237@yandex.ru>
2020-06-03 02:58:24 +0300

    Добавляет настройки персонажей

---
+++ js/setup.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var userSetup = document.querySelector('.setup');
+userSetup.classList.remove('hidden');
+
+var similarList = userSetup.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomData = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var getWizards = function (x) {
+  var wizardsArray = [];
+
+  for (var i = 0; i < x; i++) {
+    wizardsArray[i] = {
+      name: getRandomData(WIZARD_NAMES) + ' ' + getRandomData(WIZARD_SURNAMES),
+      coatColor: getRandomData(COAT_COLORS),
+      eyesColor: getRandomData(EYES_COLORS)
+    };
+  }
+
+  return wizardsArray;
+};
+
+var wizards = getWizards(4);
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarList.appendChild(fragment);
+
+userSetup.querySelector('.setup-similar').classList.remove('hidden');
